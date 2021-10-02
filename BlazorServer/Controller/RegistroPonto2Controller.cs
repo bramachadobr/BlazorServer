03ef6ddec5550077cc7242851cf4be52a479fd1e
Edivaldo Machado Barbalho <edivaldomachado@gmail.com>
2021-10-02 11:08:53 -0300

    02-10-11hs

---
+++ BlazorServer/Controller/RegistroPonto2Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorServer.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistroPonto2Controller : ControllerBase
+    {
+        [HttpGet]
+        [Route("PdfReport")]
+        public IActionResult PDFReport()
+        {
+            return null;
+        }
+
+
+
+    }
+}
